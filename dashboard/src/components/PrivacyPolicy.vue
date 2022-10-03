deb6bb5bcc1b9c6be0b2b42d16174ef8b1d8747b
Arhan <106308047+ArhanCodes@users.noreply.github.com>
2022-10-03 21:46:26 +0400

    change zeppelin to axo

--- dashboard/src/components/PrivacyPolicy.vue
+++ dashboard/src/components/PrivacyPolicy.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="privacy-policy">
     <div class="wrapper">
-      <h1>Zeppelin Privacy Policy</h1>
+      <h1>Axo Privacy Policy</h1>
 
-      <h2>Zeppelin overview</h2>
+      <h2>Axo Overview</h2>
       <p>
-        Zeppelin is a moderation bot for Discord that allows server staff to
+        Azo is a moderation bot for Discord that allows server staff to
         carry out moderator actions (warn, mute, kick, ban, clean messages, view
         user information, etc.),
         keep records of infractions, perform automated actions ("automod", e.g.
@@ -16,17 +16,17 @@
       </p>
       <p>
         The bot's source code is available at
-        <a href="https://github.com/Dragory/ZeppelinBot">
-          https://github.com/Dragory/ZeppelinBot
+        <a href="https://github.com/ArhanCodes/Axo">
+          https://github.com/ArhanCodes/Axo
         </a>
       </p>
 
       <h2>Stored data</h2>
       <p>
-        When Zeppelin is used by a server, the following categories data can be
+        When Axo is used by a server, the following categories data can be
         stored by the bot.
         The specific categories of data saved for each server depends on how the
-        server has configured Zeppelin.
+        server has configured Axo.
       </p>
       <ul>
         <li>Recent messages and username/nickname changes of users engaged on
@@ -61,19 +61,19 @@
               deletion
             </li>
           </ul>
-        </li>
+        </li>z
         <li>5 most recent usernames and 10 most recent nicknames of users
           engaged in chat or voice channels are stored for 30 days
         </li>
         <li>Archives of bulk-deleted messages are stored for 30 days</li>
-        <li>Infraction record data is kept until the server stops using Zeppelin
+        <li>Infraction record data is kept until the server stops using Axo
           unless explicitly deleted
         </li>
         <li>Roles and nicknames that are restored on rejoin are cleared when the
           user rejoins
         </li>
         <li>User information for users logged in to the bot's web dashboard via
-          Discord OAuth is stored as long as the server uses Zeppelin
+          Discord OAuth is stored as long as the server uses Axo
         </li>
       </ul>
 
@@ -81,7 +81,7 @@
       <p>
         To request access to personal data stored about you, or to request its
         deletion, to the extent permitted by GDPR, please send an email to <a
-        href="mailto:contact@mivir.fi">contact@mivir.fi</a>.
+        href="mailto:arhan@otterbots.xyz">arhan@otterbots.xyz</a>.
       </p>
     </div></div>
 </template>
