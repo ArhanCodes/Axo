7611181512d31e4a815608b87f6481bb0d655fed
Arhan <106308047+ArhanCodes@users.noreply.github.com>
2022-10-03 21:39:20 +0400

    update case icons ids

--- backend/src/plugins/Cases/caseIcons.ts
+++ backend/src/plugins/Cases/caseIcons.ts
@@ -1,13 +1,13 @@
 import { CaseTypes } from "../../data/CaseTypes";
 
 export const caseIcons: Record<CaseTypes, string> = {
-  [CaseTypes.Ban]: "<:case_ban:742540201443721317>",
-  [CaseTypes.Unban]: "<:case_unban:742540201670475846>",
+  [CaseTypes.Ban]: "<:case_ban:1026546878805987530>",
+  [CaseTypes.Unban]: "<:case_note:1026546968702488716>",
   [CaseTypes.Note]: "<:case_note:742540201368485950>",
-  [CaseTypes.Warn]: "<:case_warn:742540201624338454>",
-  [CaseTypes.Kick]: "<:case_kick:742540201661825165>",
-  [CaseTypes.Mute]: "<:case_mute:742540201817145364>",
-  [CaseTypes.Unmute]: "<:case_unmute:742540201489858643>",
-  [CaseTypes.Deleted]: "<:case_deleted:742540201473343529>",
-  [CaseTypes.Softban]: "<:case_softban:742540201766813747>",
+  [CaseTypes.Warn]: "<:case_warn:1026547060402552995>",
+  [CaseTypes.Kick]: "<:case_kick:1026547174764445767>",
+  [CaseTypes.Mute]: "<:case_mute:1026547248743600149>",
+  [CaseTypes.Unmute]: "<:case_unmute:1026547261544595486>",
+  [CaseTypes.Deleted]: "<:case_deleted:1026547273582260264>",
+  [CaseTypes.Softban]: "<:case_softban:1026547312958373900>",
 };
